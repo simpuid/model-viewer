b2f151ddb807236aac77687858fc12e2daf39b18
simpu <utkarsh.email@yahoo.com>
2019-09-28 08:31:26 +0530

    a

--- Assets/Script/BundleLoaderFile.cs
+++ Assets/Script/BundleLoaderFile.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class BundleLoaderFile : MonoBehaviour
 {
@@ -14,7 +15,13 @@ public class BundleLoaderFile : MonoBehaviour
 
     private IEnumerator Load(string bundleName,string orderFileName)
     {
-        yield return new WaitForEndOfFrame();
-        Error.ShowError(null, "Error");
+        AssetBundle bundle = AssetBundle.LoadFromFile(Path.Combine(Application.dataPath, bundleName));
+        Debug.Log(Path.Combine(Application.streamingAssetsPath, bundleName));
+        if (bundle == null)
+        {
+            Debug.Log("bundle loading failed");
+            Error.ShowError(null, "Can't load file\n"+ Path.Combine(Application.dataPath, bundleName));
+            yield break;
+        }
     }
 }
