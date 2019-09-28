4723b97239c17e4fd053e2907c9c484bf8cc7975
simpu <utkarsh.email@yahoo.com>
2019-09-28 07:45:41 +0530

    Added Bundle Loader

---
+++ Assets/Script/BundleLoaderFile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BundleLoaderFile : MonoBehaviour
+{
+    public string bundleName;
+    public string objectOrder;
+
+    private void Awake()
+    {
+        StartCoroutine(Load(bundleName, objectOrder));
+    }
+
+    private IEnumerator Load(string bundleName,string orderFileName)
+    {
+        yield return new WaitForEndOfFrame();
+    }
+}
