adb2937b31e52f4cc0a8d01d83c3d74b85169795
simpu <utkarsh.email@yahoo.com>
2019-09-28 10:40:29 +0530

    Added SocketServer

--- Assets/Script/BundleLoaderFile.cs
+++ Assets/Script/BundleLoaderFile.cs
@@ -17,14 +17,32 @@ public class BundleLoaderFile : MonoBehaviour
 
     private IEnumerator Load(string bundleName,string orderFileName)
     {
-        AssetBundle bundle = AssetBundle.LoadFromFile(Path.Combine(Application.dataPath, bundleName));
-        Debug.Log(Path.Combine(Application.streamingAssetsPath, bundleName));
+        string path = Application.persistentDataPath;
+        AssetBundle bundle = AssetBundle.LoadFromFile(Path.Combine(path, bundleName));
         if (bundle == null)
         {
-            Debug.Log("bundle loading failed");
-            Error.ShowError(null, "Can't load file\n"+ Path.Combine(Application.dataPath, bundleName));
+            Error.ShowError(null, "Bundle load fail:\n"+ Path.Combine(path, bundleName));
             yield break;
         }
+        FileObject file = FileReader.ReadFile(Path.Combine(path, orderFileName));
+        if (file == null)
+        {
+            Error.ShowError(null, "ModelList load fail\n" + Path.Combine(path, orderFileName));
+            yield break;
+        }
+        GameObject[] objects = new GameObject[file.names.Length];
+        for (int i = 0;i < file.names.Length; i++)
+        {
+            objects[i] = bundle.LoadAsset<GameObject>(file.names[i]);
+            if (objects[i] == null)
+            {
+                Error.ShowError(null, "Model load failed.\nname:" + file.names[i]);
+                yield break;
+            }
+        }
+        Bundle finalBundle = new Bundle(objects, file.names);
+        Server.bundle = finalBundle;
+        Server.fileObject = file;
         SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
     }
 }
---
+++ Assets/Script/Server.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Server : MonoBehaviour
+{
+    public static Bundle bundle;
+    public static FileObject fileObject;
+}
