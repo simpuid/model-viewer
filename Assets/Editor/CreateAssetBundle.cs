b6f25aadbed7dae370fa94f73b6997eaa1451110
simpu <utkarsh.email@yahoo.com>
2019-09-29 05:37:54 +0530

    Added Bundle Creator

---
+++ Assets/Editor/CreateAssetBundle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundle
+{
+    [MenuItem("Assets/Build Bundle")]
+    static void BuildAllAssetBundlesAndroid()
+    {
+        string assetBundleDirectoryAndroid = "Assets/AssetBundles/Android";
+        if (!Directory.Exists(assetBundleDirectoryAndroid))
+        {
+            Directory.CreateDirectory(assetBundleDirectoryAndroid);
+        }
+
+        string assetBundleDirectoryWindows = "Assets/AssetBundles/Windows";
+        if (!Directory.Exists(assetBundleDirectoryWindows))
+        {
+            Directory.CreateDirectory(assetBundleDirectoryWindows);
+        }
+
+        BuildPipeline.BuildAssetBundles(assetBundleDirectoryWindows, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        BuildPipeline.BuildAssetBundles(assetBundleDirectoryAndroid, BuildAssetBundleOptions.None, BuildTarget.Android);
+
+    }
+}
